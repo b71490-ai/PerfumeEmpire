d7065dea268ac636786051e6ce59b0459d6926c0
dev <dev@local>
2026-03-13 03:29:14 +0300

    fix(product): restore product client export wrapper to fix import path

--- frontend/app/product/ProductDetailsClient.jsx
+++ frontend/app/product/ProductDetailsClient.jsx
@@ -1,1 +1,7 @@
-export { default } from './[id]/ProductDetailsClient'
+'use client'
+
+import ProductDetailsClientInner from './[id]/ProductDetailsClient'
+
+export default function ProductDetailsClient(props) {
+  return <ProductDetailsClientInner {...props} />
+}
